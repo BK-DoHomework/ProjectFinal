89b1f200ab24033d921c21761147283d1311bcc9
trantienthuong <thuongtt@7saturday.com>
2020-02-15 23:43:50 +0700

    [1.0.0]register_part1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,37 @@
+import {validationResult} from "express-validator/check";
+import e from "express";
+
+
 let getLoginRegister =(req, res) => {
   return res.render("auth/master");
 
-}
-let getLogout =(req, res) => {
-  //do st
+};
+let postRegister =(req, res) => {
 
-}
+  // console.log(req.body);
+  let errorsArr =[];
+
+
+
+  let validationErros = validationResult(req); // khai báo biến trả về giá trị bằng giá trị gửi lên sau đó kiểm tra
+
+  if (!validationErros.isEmpty()){
+
+    let erros=Object.values(validationErros.mapped()); // => trả lại một bảng những cái lỗi đẫ đc nóm lại !
+
+    erros.forEach((item) =>{
+      errorsArr.push(item.msg); // đẩy msg vào trong 1 cái mảng rỗng đã khai báo sẵn
+    })
+    // console.log(errorsArr);
+  }
+  // console.log(req.body);
+
+
+
+};
 
 
 module.exports={
   getLoginRegister:getLoginRegister,
-  getLogout:getLogout
+  postRegister:postRegister
 }
--- src/routers/web.js
+++ src/routers/web.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import {auth, home } from "./../controllers/index";
 
+import {authValid} from "./../validation/index";
+
 
 let router = express.Router();
 /**
@@ -13,9 +15,9 @@ let initRouter = (app) => {
 
   router.get('/', home.getHome);
 
-  router.get('/login',auth.getLoginRegister );
+  router.get('/login-register',auth.getLoginRegister );
 
-  router.get('/logout', auth.getLogout);
+  router.post('/register',authValid.register, auth.postRegister); // khi kich button thì nó sẽ check dữ liệu trước khi đi qua controller
 
   return app.use("/",router);
 };
