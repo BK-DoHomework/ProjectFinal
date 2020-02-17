3be1cd8e7884d151e52f751c0b6a86f37ae9ada7
trantienthuong <thuongtt@7saturday.com>
2020-02-17 23:09:30 +0700

    [1.0.0]login_facebook

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,14 +1,18 @@
 import express from "express";
 
-import {auth, home } from "./../controllers/index";
+import { auth, home } from "./../controllers/index";
 
-import {authValid} from "./../validation/index";
+import { authValid } from "./../validation/index";
 import passport from "passport";
 import initPassportLocal from "../controllers/passportController/local";
+import initPassportFacebook from "../controllers/passportController/facebook";
+
 
 
 //init all passport
 initPassportLocal();
+
+initPassportFacebook();
 
 
 let router = express.Router();
@@ -19,25 +23,34 @@ let router = express.Router();
 
 let initRouter = (app) => {
 
-  router.get('/',auth.checkLoggedIn, home.getHome);
+  router.get('/', auth.checkLoggedIn, home.getHome);
 
-  router.get('/login-register',auth.checkLoggedOut,auth.getLoginRegister );
+  router.get('/login-register', auth.checkLoggedOut, auth.getLoginRegister);
 
-  router.post('/register',auth.checkLoggedOut,authValid.register, auth.postRegister); // khi kich button thì nó sẽ check dữ liệu trước khi đi qua controller
+  router.post('/register', auth.checkLoggedOut, authValid.register, auth.postRegister); // khi kich button thì nó sẽ check dữ liệu trước khi đi qua controller
 
-  router.get('/verify/:token',auth.checkLoggedOut, auth.verifyAccount);
+  router.get('/verify/:token', auth.checkLoggedOut, auth.verifyAccount);
 
-  router.post("/login",auth.checkLoggedOut, passport.authenticate("local",{
+  router.post("/login", auth.checkLoggedOut, passport.authenticate("local", {
     // phai dung actiom trong ejs ==> khi dung ==> tra ve
     successRedirect: "/",
-    failureRedirect:"/login-register",
+    failureRedirect: "/login-register",
     successFlash: true,
     failureFlash: true
-  }))
+  }));
 
-  router.get("/logout", auth.checkLoggedIn ,auth.getLogout);
-  return app.use("/",router);
+
+  router.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email"] }));
+
+  router.get("/auth/facebook/callback", passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login-register",
+
+  }));
+
+  router.get("/logout", auth.checkLoggedIn, auth.getLogout);
+  return app.use("/", router);
 };
 
 
-module.exports=initRouter;
+module.exports = initRouter;
